6cd4e7e2f8c57671de1b392daf809a4b6c9909b1
Mahad-007 <mahadghafoor.07@gmail.com>
2025-04-03 07:34:30 +0500

    Added and tested weather API

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 
+// console.log("Good Morning");
+// fetch("")
+// .then((res)=>res.json())
+// .then((res)=>console.log(data));
+
+const url = 'https://weather-api167.p.rapidapi.com/api/weather/current?place=London%2CGB&units=standard&lang=en&mode=json';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '4a2fb44c58mshc45e4e544c6146fp126e37jsn7066b96fc2aa',
+		'x-rapidapi-host': 'weather-api167.p.rapidapi.com',
+		Accept: 'application/json'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
